75799b204921daf01674d6564c0a2c59242da471
dalianmao <dalianmao22233@users.noreply.github.com>
2019-02-20 23:51:17 -0800

    Create 328. Odd Even Linked List.java

---
+++ 328. Odd Even Linked List.java
@@ -0,0 +1,16 @@
+注意是index为奇数偶数的重排！
+
+public class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) return null;
+        ListNode odd = head, even = head.next, evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
