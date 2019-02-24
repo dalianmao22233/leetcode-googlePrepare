fc51f1dcbd6710087996eb9de426ebf60d2be3a0
dalianmao <dalianmao22233@users.noreply.github.com>
2019-02-24 00:15:20 -0800

    Create 036. Valid Sudoku.java

---
+++ 036. Valid Sudoku.java
@@ -0,0 +1,63 @@
+1. 这个很好懂，参考：https://leetcode.com/problems/valid-sudoku/solution/， 看图就行了
+time:O(1) space:O(1),我觉得不是。。应该是三个hashmap数组
+
+class Solution {
+  public boolean isValidSudoku(char[][] board) {
+    // init data
+    HashMap<Integer, Integer> [] rows = new HashMap[9];
+    HashMap<Integer, Integer> [] columns = new HashMap[9];
+    HashMap<Integer, Integer> [] boxes = new HashMap[9];
+    for (int i = 0; i < 9; i++) {
+      rows[i] = new HashMap<Integer, Integer>();
+      columns[i] = new HashMap<Integer, Integer>();
+      boxes[i] = new HashMap<Integer, Integer>();
+    }
+
+    // validate a board
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++) {
+        char num = board[i][j];
+        if (num != '.') {
+          int n = (int)num;
+          int box_index = (i / 3 ) * 3 + j / 3;
+
+          // keep the current cell value
+          rows[i].put(n, rows[i].getOrDefault(n, 0) + 1);
+          columns[j].put(n, columns[j].getOrDefault(n, 0) + 1);
+          boxes[box_index].put(n, boxes[box_index].getOrDefault(n, 0) + 1);
+
+          // check if this value has been already seen before
+          if (rows[i].get(n) > 1 || columns[j].get(n) > 1 || boxes[box_index].get(n) > 1)
+            return false;
+        }
+      }
+    }
+
+    return true;
+  }
+}
+
+
+2. 
+Collect the set of things we see, encoded as strings. For example:
+
+'4' in row 7 is encoded as "(4)7".
+'4' in column 7 is encoded as "7(4)".
+'4' in the top-right block is encoded as "0(4)2".
+Scream false if we ever fail to add something because it was already added (i.e., seen before).
+这种做饭就是把括号换成了"in row" "in column"... ， 在set中看是否有相同的string. 
+
+public boolean isValidSudoku(char[][] board) {
+    Set seen = new HashSet();
+    for (int i=0; i<9; ++i) {
+        for (int j=0; j<9; ++j) {
+            char number = board[i][j];
+            if (number != '.')
+                if (!seen.add(number + " in row " + i) ||
+                    !seen.add(number + " in column " + j) ||
+                    !seen.add(number + " in block " + i/3 + "-" + j/3))
+                    return false;
+        }
+    }
+    return true;
+}
