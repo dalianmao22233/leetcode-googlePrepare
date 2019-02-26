c32abede807b526b9272b74cdf3a25824fc376bb
dalianmao <dalianmao22233@users.noreply.github.com>
2019-02-25 23:47:06 -0800

    Update and rename 240. Search a 2D Matrix II to 240. Search a 2D Matrix II.java

--- 240. Search a 2D Matrix II.java
+++ 240. Search a 2D Matrix II.java
@@ -1,5 +1,8 @@
 //从右上角往下搜就是找更大的，往左搜就是找更小的，或者可以从左下角开始。
 //时间复杂度：o(m+n) 空间复杂度：o(1)
+
+这道题是从右上角开始，如果> taraget, 向左看， < target， 向下看
+
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         if (matrix == null || matrix.length == 0) {
@@ -21,3 +24,6 @@ class Solution {
         return false;
     }
 }
+
+
+https://leetcode.com/problems/search-a-2d-matrix-ii/solution/ 还有其他解法，binary search, divide and conquer等
