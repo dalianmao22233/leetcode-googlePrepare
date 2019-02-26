60d4decb91af63b1360030c751ad8f8b3ab0e6f3
dalianmao <dalianmao22233@users.noreply.github.com>
2019-02-25 21:20:17 -0800

    Update 369. Plus One Linked List.java

--- 369. Plus One Linked List.java
+++ 369. Plus One Linked List.java
@@ -1,11 +1,4 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
+1. 常规解法，2 pointer
 class Solution {
     public ListNode plusOne(ListNode head) {
         ListNode dummy = new ListNode(0);
@@ -23,9 +16,7 @@ class Solution {
         if (j.val != 9) {   // 最后一个点小于9. 
             j.val++;
         } else {     // 最后一个点==9， 
-            System.out.println(i.val);
             i.val++;
-            System.out.println(i.val);
             i = i.next;
             while (i != null) {
                 i.val = 0;   // 最后一位设置为0. （10）
@@ -38,4 +29,28 @@ class Solution {
         }
         return dummy;    // 是9->9->9的情况。
     }
+}
+
+2. recursive：
+
+public ListNode plusOne(ListNode head) {
+    if( DFS(head) == 0){
+        return head;
+    }else{
+        ListNode newHead = new ListNode(1);
+        newHead.next = head;
+        return newHead;
+    }
+}
+
+public int DFS(ListNode head){
+    if(head == null) return 1;
+    
+    int carry = DFS(head.next);
+    
+    if(carry == 0) return 0;
+    
+    int val = head.val + 1;
+    head.val = val%10;
+    return val/10;
 }
