8dcc1edcafdfe1bdfc847d4632017a6325e9acc1
dalianmao <dalianmao22233@users.noreply.github.com>
2019-02-26 00:37:28 -0800

    Create 451. Sort Characters By Frequency.java

---
+++ 451. Sort Characters By Frequency.java
@@ -0,0 +1,50 @@
+1. pq
+according to user "orxanb", O(nlogm), m is the distinguish character, can be O(1) since only 26 letters. 
+So the overall time complexity should be O(n), the same as the buck sort with less memory use.
+
+public class Solution {
+    public String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray())
+            map.put(c, map.getOrDefault(c, 0) + 1);
+						
+        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        pq.addAll(map.entrySet());
+				
+        StringBuilder sb = new StringBuilder();
+        while (!pq.isEmpty()) {
+            Map.Entry e = pq.poll();
+            for (int i = 0; i < (int)e.getValue(); i++) 
+                sb.append(e.getKey());
+        }
+        return sb.toString();
+    }
+}
+
+2. bucket sort
+here we just use a map a count and according to the frequency to put it into the right bucket. 
+Then we go through the bucket to get the most frequently character and append that to the final stringbuilder.
+
+public class Solution {
+    public String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) 
+            map.put(c, map.getOrDefault(c, 0) + 1);
+						
+        List<Character> [] bucket = new List[s.length() + 1];
+        for (char key : map.keySet()) {
+            int frequency = map.get(key);
+            if (bucket[frequency] == null) bucket[frequency] = new ArrayList<>();
+            bucket[frequency].add(key);
+        }
+				
+        StringBuilder sb = new StringBuilder();
+        for (int pos = bucket.length - 1; pos >= 0; pos--)
+            if (bucket[pos] != null)
+                for (char c : bucket[pos])
+                    for (int i = 0; i < map.get(c); i++)
+                        sb.append(c);
+
+        return sb.toString();
+    }
+}
