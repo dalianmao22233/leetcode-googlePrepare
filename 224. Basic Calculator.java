9275e333eb769467e55f124dd973949a50d7a3d7
dalianmao <dalianmao22233@users.noreply.github.com>
2019-03-03 21:40:38 -0800

    Update 224. Basic Calculator.java

--- 224. Basic Calculator.java
+++ 224. Basic Calculator.java
@@ -75,3 +75,37 @@ class Solution {
         return result;
     }
 }
+
+3. 和变种III思路一致：
+在做了Basic Calculator III之后，再反过头来看这道题，发现递归处理括号的方法在这道题也同样适用，
+我们用一个变量cnt，遇到左括号自增1，遇到右括号自减1，
+当cnt为0的时候，说明括号正好完全匹配，
+这个trick在验证括号是否valid的时候经常使用到。
+然后我们就是根据左右括号的位置提取出中间的子字符串调用递归函数，返回值赋给num
+
+class Solution {
+public:
+    int calculate(string s) {
+        int res = 0, num = 0, sign = 1, n = s.size();
+        for (int i = 0; i < n; ++i) {
+            char c = s[i];
+            if (c >= '0' && c <= '9') {
+                num = 10 * num + (c - '0');
+            } else if (c == '(') {
+                int j = i, cnt = 0;
+                for (; i < n; ++i) {
+                    if (s[i] == '(') ++cnt;
+                    if (s[i] == ')') --cnt;
+                    if (cnt == 0) break;
+                }
+                num = calculate(s.substr(j + 1, i - j - 1));
+            }
+            if (c == '+' || c == '-' || i == n - 1) {
+                res += sign * num;
+                num = 0;
+                sign = (c == '+') ? 1 : -1;
+             } 
+        }
+        return res;
+    }
+};
