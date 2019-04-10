17b54d09e74af4c715ecc81b28daafb116154647
dalianmao <dalianmao22233@users.noreply.github.com>
2019-04-09 17:33:14 -0700

    Update Sort with 3 Stacks.java

--- Sort with 3 Stacks.java
+++ Sort with 3 Stacks.java
@@ -1,11 +1,22 @@
 // laioffer
 // 1. 孙老师做法：
 
+思想就是：
+s1 // 4 3 1 2 5
+s2 // 
+s3 //
+
+维护一个min, 每次找到s1中的min, 然后s1中所有元素放到s2中，
+把s2的栈顶与min比较，如果是min, 就放到s3中，不是的话就放回s1.
+执行以上2个操作直到s1为空。此时s3是有序的，从栈顶到栈底为降序。
+把s3放回s1. 此时是升序的。
+
+
 public class Solution {
   public void sort(LinkedList<Integer> s1) {
   
   // 还可以写成：
- ## // Deque<Integer> s = new LinkedList<>();  因为LinkedList(以及ArrayDeque)是Deque接口的实现。
+  // Deque<Integer> s = new LinkedList<>();  因为LinkedList(以及ArrayDeque)是Deque接口的实现。
     LinkedList<Integer> s2 = new LinkedList<Integer>();
     LinkedList<Integer> s3 = new LinkedList<Integer>();
     // Write your solution here.
