085aeb42243855f50974d92237a2d306a0d57c47
dalianmao <dalianmao22233@users.noreply.github.com>
2021-02-01 18:30:53 -0800

    Update 287. Find the Duplicate Number.java

--- 287. Find the Duplicate Number.java
+++ 287. Find the Duplicate Number.java
@@ -1,3 +1,52 @@
+https://github.com/wisdompeak/LeetCode/tree/master/Binary_Search/287.Find-the-Duplicate-Number
+典型的2pointer：面试先写这个。O(nlogn)
+想法：跟数组index没关系，mid是1-n的中间数。 如果<=mid的count 大于mid，则说明目标所在范围在[1-mid], 反之则在[mid+1, n]
+为什么low=mid+1: count已经算了比mid小于等于的数，包含了mid. 所以mid一定不是结果。
+举例 9,9,9,9,9,9,9,9,9
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+	    int low = 1, high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) /2;
+            System.out.println("l: " + low + ", h: " + high);
+            System.out.println("mid: " + mid);
+            int cnt = 0;
+            for (int a : nums) {
+                if (a <= mid) ++cnt;
+            }
+            System.out.println(cnt);
+            if (cnt <= mid) low = mid + 1;
+            else high = mid;
+        }
+        return low;
+    }
+}
+
+解法2：indexing sort
+利用indexing sort的方法，我们尝试将n+1个数组元素尽可能地按照“index==value”的方法重新放置在1~n+1这些位置上。放置完之后，必然是有若干个位置的index==val，而有某些index对应的nums[index]!=index，并且这些位置上的数值都是duplicated number。
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) 
+    {
+        int n = nums.size()-1;
+        nums.insert(nums.begin(), 0);
+        for (int i=1; i<=n+1; i++)
+        {
+            while (nums[i]!=i && nums[i]<=n+1 && nums[i]!=nums[nums[i]])
+                swap(nums[i], nums[nums[i]]);
+        }
+        for (int i=1; i<=n+1; i++)
+        {
+            if (nums[i]!=i)
+                return nums[i];
+        }
+        return -1;
+    }
+};
+
+
 1. sort : O(nlogn) 先答这种，cycle detection 可能面试官会觉得我见过这个题
 
 class Solution {
