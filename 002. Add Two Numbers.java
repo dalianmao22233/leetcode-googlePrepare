fd75fa7bfce48be45335ede40da2329b5a5212d2
dalianmao <dalianmao22233@users.noreply.github.com>
2021-03-08 23:13:41 -0800

    Update 002. Add Two Numbers.java

--- 002. Add Two Numbers.java
+++ 002. Add Two Numbers.java
@@ -1,55 +1,24 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
-class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if (l1 == null && l2 == null) {
-            return null;
-        }
-        if (l1 == null) {
-            return l2;
-        }
-        if (l2 == null) {
-            return l1;
-        }
-        
-        ListNode dummyNode = new ListNode(0);
-        ListNode cur = dummyNode;
-        int carry = 0;
-        
-        while (l1 != null || l2 != null) {
-            int val = 0;
-            if (l1 != null) {
-                val += l1.val;
-            } 
-            if (l2 != null) {
-                val += l2.val;
-            }
-            val += carry;
-        
-            carry = val/10;
-            val = val%10;
+高级 太流畅了！
 
-            cur.next = new ListNode(val);
+class Solution {
+   public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;  // cur 存尾节点
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
             if (l1 != null) {
+                carry += l1.val;
                 l1 = l1.next;
             }
             if (l2 != null) {
+                carry += l2.val;
                 l2 = l2.next;
             }
-            
+
+            cur.next = new ListNode(carry % 10);
+            carry /= 10;
             cur = cur.next;
         }
-        
-        if (carry > 0) {
-            cur.next = new ListNode(carry);
-        }
-    
-        return dummyNode.next;
+        return dummy.next;
     }
 }
