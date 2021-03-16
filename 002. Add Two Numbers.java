963f534c0125cac1e3bd21bd1e6bbe9c3598b516
dalianmao <dalianmao22233@users.noreply.github.com>
2021-03-15 21:38:40 -0700

    Update 002. Add Two Numbers.java

--- 002. Add Two Numbers.java
+++ 002. Add Two Numbers.java
@@ -6,6 +6,10 @@ class Solution {
         ListNode cur = dummy;  // cur 存尾节点
         int carry = 0;
         while (l1 != null || l2 != null || carry != 0) {
+        // 为什么要carry !=0 ： 
+         //[9,9,9,9,9,9,9]
+         //[9,9,9,9]
+         //[8,9,9,9,0,0,0,1] 为正确结果，如果没有carry的条件，10/10=1, 最后一个1就没法得到。
             if (l1 != null) {
                 carry += l1.val;
                 l1 = l1.next;
