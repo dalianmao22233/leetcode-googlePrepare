16b397979e68c249f125063879a350ab36c5000c
dalianmao <dalianmao22233@users.noreply.github.com>
2021-03-31 14:57:23 -0700

    Update 035. Search Insert Position.java

--- 035. Search Insert Position.java
+++ 035. Search Insert Position.java
@@ -1,45 +1,20 @@
-my solution:
-
+acwing: 模板
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        if (nums == null || nums.length == 0) {
-            return -1;
-        }
-        int left = 0;
-        int right = nums.length-1;
-        while (left < right-1) {   // 来offer的模板，left right相邻的时候停下来，这就是target的大概位置了，
-        // 1. left, right 之间是target, 
-        // 2. right 右边是target
-        // 3. left 左边是target
+        int l = 0; 
+        int r = nums.length; //注意这里不要-1， 因为可能最后插入到最后一个元素后面，越界了
         
-            int mid = left + (right-left)/2;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] > target) {
-                right = mid;
+        while (l < r) {
+            int mid = l+r >> 1;
+            if (nums[mid] >= target) {
+                r = mid;
             } else {
-                left = mid;
+                l = mid+1;
             }
         }
-        // System.out.println(left + "," + right);
-        
-        if (target == nums[left]) {
-            return left; // [1,3], target=1
-        }
-        if (target == nums[right]) {
-            return right; // [1,3], target=3
-        }
-        
-        if (target > nums[left] && target < nums[right]) {
-            return right;
-        }
-        if (target > nums[left] && target > nums[right]) {
-            return right+1;
-        }
-        return left;  // 都不满足就放左边呗。。 就是target比nums[left]小
+        return l;
     }
 }
-
 
 2. 更简洁：但是我不懂 还是用第一种吧
 
